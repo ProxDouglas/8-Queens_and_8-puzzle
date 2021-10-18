ccc55b6e87fffcc8ba7a182b4bbf93c64c15343f
Lucas Stefenoni de Aquino Pereira <lucassap01@gmail.com>
2021-10-17 22:37:54 -0300

    Implementada contagem do AStar

--- 8-queens/A_Star/functions.py
+++ 8-queens/A_Star/functions.py
@@ -2,17 +2,18 @@ import numpy as np
 
 def attacked_queens_pairs(seqs):
     """
+    
     Calculate the queen logarithm of the attack on the chessboard corresponding to the sequence n]
     We only need to check whether there are other queens on the eight queens of the current chessboard in their respective rows and two diagonals, without judging whether there are other queens in the same column
     """
+    tentativas = 0
     a = np.array([0] * 81)  # Create a one-dimensional array with 81 zeros
     a = a.reshape(9, 9)  # Change to 9 * 9 two-dimensional array. For the convenience of later use, only the 8 * 8 parts of the last eight rows and columns are used as a blank chessboard
     n = 0  # The number of queens attacking each other is initialized to 0
-
     for i in range(1, 9):
         if seqs[i-1] != 0: # An element of seqs is 0, which means that no queen should be placed in the corresponding chessboard column
             a[seqs[i - 1]][i] = 1  # Generate the corresponding chessboard sequence, and place it in the order of the first chessboard column
-
+            #tentativas = tentativas + 1
     for i in range(1, 9):
         if seqs[i - 1] == 0:
             continue # If an element of seqs is 0, it represents the corresponding chessboard. If no queen is placed in this column, the next column will be judged directly
@@ -21,6 +22,7 @@ def attacked_queens_pairs(seqs):
                 n += 1
         t1 = t2 = seqs[i - 1]
         for j in range(i - 1, 0, -1):  # Look at the two diagonals in the left half
+            tentativas = tentativas + 1
             if t1 != 1:
                 t1 -= 1
                 if a[t1][j] == 1:
@@ -32,7 +34,8 @@ def attacked_queens_pairs(seqs):
                     n += 1  # There are other queens on the left half of the sub diagonal
 
         t1 = t2 = seqs[i - 1]
-        for j in range(i + 1, 9):  # Look at the two diagonals in the right half
+        for j in range(i + 1, 9):
+            tentativas = tentativas + 1  # Look at the two diagonals in the right half
             if t1 != 1:
                 t1 -= 1
                 if a[t1][j] == 1:
@@ -42,7 +45,8 @@ def attacked_queens_pairs(seqs):
                 t2 += 1
                 if a[t2][j] == 1:
                     n += 1  # There are other queens on the right half of the sub diagonal
-    return int(n/2)  # Returns n/2, because A attacking B also means B attacking A, so returns half of n
+    print("Tentativas = " + (str(tentativas)))
+    return int(n/2), tentativas  # Returns n/2, because A attacking B also means B attacking A, so returns half of n
 
 def display_board(seqs):
     """
